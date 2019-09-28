96f84cacd8c12ef67cde9111bdda97ffa3c9f03a
ruixinl6 <44179096+ruixinl6@users.noreply.github.com>
2019-09-28 00:47:41 -0400

    Update rl.py

--- hw2-VI-PI-DQN/Q2-VI-PI/deeprl_hw2q2/rl.py
+++ hw2-VI-PI-DQN/Q2-VI-PI/deeprl_hw2q2/rl.py
@@ -6,6 +6,7 @@ from __future__ import print_function, unicode_literals
 import numpy as np
 import deeprl_hw2q2.lake_envs as lake_env
 
+#yufan
 
 def print_policy(policy, action_names):
     """Print the policy in human-readable format.
