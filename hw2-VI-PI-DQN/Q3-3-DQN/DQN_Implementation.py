12e29b86b484980097dc3647308d6b0a53fea5b0
Dyyo <35265978+Dyyo24@users.noreply.github.com>
2019-10-02 21:17:05 -0400

    DQN Wednesday

--- hw2-VI-PI-DQN/Q3-3-DQN/DQN_Implementation.py
+++ hw2-VI-PI-DQN/Q3-3-DQN/DQN_Implementation.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
-import keras, tensorflow as tf, numpy as npy, gym, sys, copy, argparse
+import keras, tensorflow as tf, numpy as np, gym, sys, copy, argparse
+from keras.models import Sequential
+from keras.layers import Dense
+from keras import metrics
+
+import collections
+import os
 
 class QNetwork():
 
@@ -7,29 +13,37 @@ class QNetwork():
 	# The network should take in state of the world as an input, 
 	# and output Q values of the actions available to the agent as the output. 
 
-	def __init__(self, environment_name):
+    def __init__(self, state_dim,action_dim,learning_rate):
 		# Define your network architecture here. It is also a good idea to define any training operations 
 		# and optimizers here, initialize your variables, or alternately compile your model here.  
-		pass
-
-	def save_model_weights(self, suffix):
+        self.model = Sequential()       # the model that is trained for Q value estimator (Q hat)
+        self.model.add(Dense(25, activation='tanh', input_shape=(state_dim+1,)))  # input: state and action
+        self.model.add(Dense(80,activation='tanh'))
+        self.model.add(Dense(1, activation='softmax'))
+        self.model.compile(loss='mse', optimizer=keras.optimizers.Adam(learning_rate=learning_rate), metrics=['mape'])
+          
+    def save_model_weights(self, suffix):
 		# Helper function to save your model / weights. 
-		pass
-
-	def load_model(self, model_file):
+        keras.models.save_model(suffix,self.model.get_weights())    		
+                    # save #np.save(suffix,self.model.get_weights())
+                    
+    def load_model(self, model_file):
 		# Helper function to load an existing model.
 		# e.g.: torch.save(self.model.state_dict(), model_file)
-		pass
-
-	def load_model_weights(self,weight_file):
+        
+        return keras.models.load_model(model_file)    
+                
+    def load_model_weights(self,weight_file):
 		# Helper funciton to load model weights. 
 		# e.g.: self.model.load_state_dict(torch.load(model_file))
-		pass
+		
+        return keras.models.load_model(weight_file) 
+
 
 
 class Replay_Memory():
 
-	def __init__(self, memory_size=50000, burn_in=10000):
+    def __init__(self,env, memory_size=50000, burn_in=10000):
 
 		# The memory essentially stores transitions recorder from the agent
 		# taking actions in the environment.
@@ -37,20 +51,44 @@ class Replay_Memory():
 		# Burn in episodes define the number of episodes that are written into the memory from the 
 		# randomly initialized agent. Memory size is the maximum size after which old elements in the memory are replaced. 
 		# A simple (if not the most efficient) was to implement the memory is as a list of transitions. 
-		
-		# Hint: you might find this useful:
-		# 		collections.deque(maxlen=memory_size)
-	 	pass
+        self.memory_size = memory_size
+        self.Transition = collections.namedtuple("Transition", ["state", "action", "reward", "next_state", "done"])
+        self.M = collections.deque(maxlen=memory_size)  # list-like container with fast appends and pops on either end
+        state = env.reset()                   # observation of initial state
+        next_state = state.copy()             # initialize next state
+        
+        while len(self.M) < burn_in:         # while the size of memory does not exceed burnin size
+            done = False
+            while not done:
+                action = env.action_space.sample() 
+                state = next_state.copy()
+                next_state, reward, done, info = env.step(action)
+                
+                self.append(self.Transition(state,action,reward,next_state,done))
 
-	def sample_batch(self, batch_size=32):
+        
+    def sample_batch(self, batch_size=32):
 		# This function returns a batch of randomly sampled transitions - i.e. state, action, reward, next state, terminal flag tuples. 
 		# You will feed this to your model to train.
-		pass
+        
+        size = len(self.M)      # the size of M        
+    #    print('size M: ',size)
+        random_list = np.random.permutation(size)
+    #    print('section',random_list[0:31])
+        batch = []
+        for i in random_list[0:32]:
+            batch.append(self.M[i])
+ 
+      #  batch = self.M[random_list[0:31]].copy()
+        return batch
+        
+        
 
-	def append(self, transition):
+    def append(self, transition):
 		# Appends transition to the memory. 	
-		pass
-
+        self.M.append(transition)
+        if len(self.M) > self.memory_size:
+            self.M.popleft()
 
 class DQN_Agent():
 
@@ -63,38 +101,119 @@ class DQN_Agent():
 	# (4) Create a function to test the Q Network's performance on the environment.
 	# (5) Create a function for Experience Replay.
 	
-	def __init__(self, environment_name, render=False):
+    def __init__(self, environment_name,episode, epsilon, gamma, C, learning_rate,render=False,burn_in = 10000,memory_size=50000):
 
 		# Create an instance of the network itself, as well as the memory. 
 		# Here is also a good place to set environmental parameters,
 		# as well as training parameters - number of episodes / iterations, etc. 
-		pass 
-
-	def epsilon_greedy_policy(self, q_values):
+        self.env = gym.make(environment_name)
+        print('state',self.env.observation_space.shape[0])
+        print('action',self.env.action_space.n)
+        self.Qnet = QNetwork(self.env.observation_space.shape[0],self.env.action_space.n, learning_rate)
+        self.Qnet_target = QNetwork(self.env.observation_space.shape[0],self.env.action_space.n, learning_rate)
+        
+        self.Replay = Replay_Memory(self.env, memory_size, burn_in)
+        self.Transition = self.Replay.Transition
+        
+        
+        
+        self.gamma = gamma           # discount factor
+        self.episode = episode       # number of episodes to run 
+        self.memory = self.Replay.M  # memory object list of tuple size (n,5)
+        self.eps = epsilon
+        self.C = C
+        self.learning_rate = learning_rate
+    
+    def epsilon_greedy_policy(self, q_values):
 		# Creating epsilon greedy probabilities to sample from.             
-		pass
+        if np.random.randn(1) < self.eps:             # input a list of qvalues with all action space 
+            action = self.env.action_space.sample()   # and output the action
+        else:
+            action = np.argmax(q_values)
+        return action
+        
 
-	def greedy_policy(self, q_values):
+    def greedy_policy(self, q_values):
 		# Creating greedy policy for test time. 
-		pass 
+        action = np.argmax(q_values)
+        return action
 
-	def train(self):
+    def train(self):
 		# In this function, we will train our network. 
 		# If training without experience replay_memory, then you will interact with the environment 
 		# in this function, while also updating your network parameters. 
-
 		# When use replay memory, you should interact with environment here, and store these 
 		# transitions to memory, while also updating your model.
-		pass
+        loss = []
+    #    acc = []
+        count = 0 
+        for i in range(self.episode):
+            state = self.env.reset()
+            size = state.shape[0]
+            done = False
+            c = 0
+            print('one episode!')
+            while not done:
+                                   
+            #    print('size',size)
+            #    print('test',np.concatenate((state.copy(),[1])).shape)
+                greedy = []
+                for act in range(self.env.action_space.n):
+           #         print('act',act)
+                    greedy.append(self.Qnet.model.predict(np.concatenate((state.copy(),[act])).reshape(1,size+1)).tolist()[0][0] )
+                    
+           #     print('greedy',greedy)    
+                action = self.epsilon_greedy_policy(greedy)      # epsilon greedy policy
+                next_state, reward, done, info = self.env.step(action)
+                self.Replay.append(self.Transition(state,action,reward,next_state,done))   # store transition in memory
+                batch = self.Replay.sample_batch(batch_size=32)       # sample minibatch from memory size(32,5)
+                y = []
+                x = []
+                qnet = []
+                for transition in batch:
+                    state_x = transition[0].copy()
+                    act_x = transition[1]
+                    x.append(np.concatenate((state_x,[act_x])))
+                    r = transition[2]  # reward
+                    s = transition[0]  # state
+                    qnet.append(self.Qnet.model.predict(np.concatenate((s.copy(),[act_x])).reshape(1,size+1)).tolist()[0][0])
+                    if transition[4] == True:      # if done
+                        y.append(r)    # append reward
+                    else:
+                        
+                        greedy = []
+                        for act in range(self.env.action_space.n):
+                            greedy.append(self.Qnet_target.model.predict(np.concatenate((s.copy(),[act])).reshape(1,size+1)).tolist()[0][0])
+                        
+                        a_opt = self.greedy_policy([greedy])   
+                        y.append(r+self.gamma*self.Qnet_target.model.predict(np.concatenate((s.copy(),[a_opt])).reshape(1,size+1)).tolist()[0][0]) 
+                
+                error = np.array(y) - np.array(qnet) 
+                
+                history = self.Qnet.model.fit(np.array(x),error,epochs = 10,verbose=0)
+                loss.append( history.history['mape'] )
+           #     acc.append( history.history['accuracy'] )
+                c+=1 
+                if c % self.C == 0:
+                    self.Qnet_target.model.set_weights(self.Qnet.model.get_weights())
+                
+        return loss
 
-	def test(self, model_file=None):
+    def test(self, model_file=None):
 		# Evaluate the performance of your agent over 100 episodes, by calculating cummulative rewards for the 100 episodes.
 		# Here you need to interact with the environment, irrespective of whether you are using a memory. 
-
-	def burn_in_memory():
+        
+        
+        
+        
+        pass
+     
+    def burn_in_memory():
 		# Initialize your replay memory with a burn_in number of episodes / transitions. 
-		pass
-
+#        Replay = Replay_Memory(self.env,memory_size, burn_in)
+        pass
+        #return Replay
+        
 
 # Note: if you have problems creating video captures on servers without GUI,
 #       you could save and relaod model to create videos on your laptop. 
@@ -104,7 +223,7 @@ def test_video(agent, env, epi):
 	# 		if episode % int(self.num_episodes/3) == 0:
     #       	test_video(self, self.environment_name, episode)
     save_path = "./videos-%s-%s" % (env, epi)
-    if not os.pa th.exists(save_path):
+    if not os.path.exists(save_path):
         os.mkdir(save_path)
     # To create video
     env = gym.wrappers.Monitor(agent.env, save_path, force=True)
@@ -132,19 +251,24 @@ def parse_arguments():
 
 def main(args):
 
-	args = parse_arguments()
-	environment_name = args.env
+    args = parse_arguments()
+    environment_name = args.env
 
 	# Setting the session to allow growth, so it doesn't allocate all GPU memory. 
-	gpu_ops = tf.GPUOptions(allow_growth=True)
-	config = tf.ConfigProto(gpu_options=gpu_ops)
-	sess = tf.Session(config=config)
+    gpu_ops = tf.GPUOptions(allow_growth=True)
+    config = tf.ConfigProto(gpu_options=gpu_ops)
+    sess = tf.Session(config=config)
 
 	# Setting this as the default tensorflow session. 
-	keras.backend.tensorflow_backend.set_session(sess)
+    keras.backend.tensorflow_backend.set_session(sess)
 
 	# You want to create an instance of the DQN_Agent class here, and then train / test it. 
-
+    
+    env_name = 'MountainCar-v0'
+    a = DQN_Agent(env_name,episode = 1000, epsilon=0.05 , gamma=1, C = 5)
+    a.train()
+    
+    
 if __name__ == '__main__':
 	main(sys.argv)
 
